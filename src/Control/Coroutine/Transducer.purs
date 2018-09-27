4fafbbfc9d654146638e783c7e28f61c0a061f79
blinky3713 <blinky3713@gmail.com>
2018-09-27 13:06:45 -0700

    added comments

--- src/Control/Coroutine/Transducer.purs
+++ src/Control/Coroutine/Transducer.purs
@@ -24,7 +24,7 @@ import Control.Monad.Free.Trans (freeT, interpret, resume)
 import Control.Monad.Rec.Class (class MonadRec)
 import Control.Monad.Trans.Class (lift)
 import Control.Parallel (class Parallel, parallel, sequential)
-import Data.Either (Either(..))
+import Data.Either (Either(..), either)
 import Data.Functor.Coproduct (Coproduct(..), left, right)
 import Data.Maybe (Maybe(..), maybe)
 import Data.Traversable (traverse_)
@@ -35,6 +35,11 @@ import Unsafe.Coerce (unsafeCoerce)
 
 type Transducer i o = Co (Coproduct ((->) (Maybe i)) (Tuple o))
 
+-- | The `fuse` operator runs an upstream and downstream `Transducer` in parallel. In the event that
+-- | the upstream pauses with an emit statement and the downstream with an await statement,
+-- | data is passed downstream and the computation resumed. In the event that they have both terminated,
+-- | we return both termination and terminate. Otherwise we build a continuation which can be resumed later
+-- | and makes use of the accumulated state.
 fuse
   :: forall a b c m par x y.
      MonadRec m
@@ -58,9 +63,11 @@ fuse t1 t2 = freeT \_ -> go (Tuple t1 t2)
 
 infixr 2 fuse as =>=
 
+-- | Await an upstream value.
 awaitT :: forall i o m. Monad m => Transducer i o m (Maybe i)
 awaitT = freeT $ \_ -> pure <<< Right $ (left pure)
 
+-- | Continue waiting for upstream values, while never returning.
 awaitForever :: forall i o m r. Monad m => (i -> Transducer i o m r) -> Transducer i o m Unit
 awaitForever send = loop do
   mi <- awaitT
@@ -68,20 +75,22 @@ awaitForever send = loop do
     Nothing -> pure $ Just unit
     Just i -> send i $> Nothing
 
+-- | Yield a value to downstream
 yieldT :: forall i o m. Monad m => o -> Transducer i o m Unit
 yieldT o = freeT $ \_ -> pure <<< Right $ (right $ Tuple o (pure unit))
 
+-- | Create a `Transducer` from a pure function.
 transform :: forall i o m. Monad m => (i -> o) -> Transducer i o m Unit
 transform f = transformM (pure <<< f)
 
+-- | Create a `Transducer` from an effectful function.
 transformM :: forall i o m. Monad m => (i -> m o) -> Transducer i o m Unit
 transformM f = do
   mi <- awaitT
   maybe (pure unit) (\i -> lift (f i) >>= yieldT) mi
 
-
 --------------------------------------------------------------------------------
--- | Stateful pipelines
+--  Stateful pipelines
 --------------------------------------------------------------------------------
 
 liftStateless :: forall i o m. Monad m => (i -> Array o) -> Transducer i o m Unit
@@ -89,6 +98,7 @@ liftStateless f = do
   mi <- awaitT
   maybe (pure unit) (\i -> traverse_ yieldT (f i) >>= \_ -> liftStateless f) mi
 
+-- | Transform values according to an accumulated state `s`.
 liftStateful :: forall i o m s. Monad m => (s -> i -> Tuple s (Array o)) -> (s -> Array o) -> s -> Transducer i o m Unit
 liftStateful f eof s = do
   mi <- awaitT
@@ -96,7 +106,7 @@ liftStateful f eof s = do
                                           in traverse_ yieldT bs >>= \_ -> liftStateful f eof s') mi
 
 --------------------------------------------------------------------------------
--- | Interactions with Control.Coroutine `Producer`s and `Consumer`s
+-- Interactions with Control.Coroutine `Producer`s and `Consumer`s
 --------------------------------------------------------------------------------
 
 fromProducer :: forall m o r. Monad m => Producer o m r -> Transducer Void o m r
@@ -115,24 +125,24 @@ toConsumer = interpret (unsafePartial $ case _ of
 
 fromTransformer :: forall i o m x. MonadRec m => Transformer i o m x -> Transducer i o m Unit
 fromTransformer t = do
-  r <- lift $ resume t
-  case r of
-    Left _ -> pure unit
-    Right (Transform f) -> do
-      mi <- awaitT
-      case mi of
-        Nothing -> pure unit
-        Just i -> do
-          let Tuple o k = f i
-          yieldT o
-          fromTransformer k
+    a <- lift $ resume t
+    either (const $ pure unit) go a
+  where
+   go (Transform f) = do
+     mi <- awaitT
+     case mi of
+       Nothing -> pure unit
+       Just i -> do
+         let Tuple o k = f i
+         yieldT o
+         fromTransformer k
 
 fromCoTransformer :: forall i o m x. MonadRec m => CoTransformer i o m x -> Transducer i o m Unit
 fromCoTransformer t = do
-  r <- lift $ resume t
-  case r of
-    Left _ -> pure unit
-    Right (CoTransform o f) -> do
+    a <- lift $ resume t
+    either (const $ pure unit) go a
+  where
+    go (CoTransform o f) = do
       yieldT o
       mi <- awaitT
       maybe (pure unit) (fromCoTransformer <<< f) mi
