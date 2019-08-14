2fe54fa48592d89d6b2e1e35df9e88bfce3c9bc6
Irakli Safareli <i.safareli@gmail.com>
2019-08-14 19:25:45 +0400

    fix warning

--- src/Control/Coroutine/Transducer.purs
+++ src/Control/Coroutine/Transducer.purs
@@ -58,7 +58,7 @@ fuse t1 t2 = freeT \_ -> go (Tuple t1 t2)
     proceed c (Right (Coproduct (Right s))) = pure (Right $ right $ map (fuse $ freeT $ \_ -> pure c) s)
     proceed (Right (Coproduct (Right (Tuple o next)))) (Right (Coproduct (Left f))) = resume (next `fuse` f (Just o))
     proceed (Right (Coproduct (Right (Tuple o next)))) (Left y) = resume (next `fuse` pure y)
-    proceed (Left x) (Right (Coproduct (Left f))) = resume (pure x `fuse` f Nothing)
+    proceed (Left z) (Right (Coproduct (Left f))) = resume (pure z `fuse` f Nothing)
     proceed (Left x) (Left y) = pure <<< Left $ (Tuple x y)
 
 infixr 2 fuse as =>=
